c87c110b7bb72fec08bc1671ae4d49a278518197
HenrikB <92089215+henrikboren@users.noreply.github.com>
2023-12-08 12:12:06 +0100

    move

--- script.js
+++ script.js
@@ -1,17 +1,63 @@
+let mousedown = false;
+let isFlipped = false;
+let currentTeacher = 0;
+let mousedownX;
+let mouseupX;
+const dragActionDistance = 100;
+const body = document.body;
 const teacherCard = document.querySelectorAll(".teacherCard");
 const teacherCardBack = teacherCard[0].cloneNode(false);
 
+teacherCard[currentTeacher].style.transition = "transform 0s ease-in-out";
+
+teacherCardBack.addEventListener("click", flipteacherCard);
+for (let i = 0; i < teacherCard.length; i++) {
+	teacherCard[i].style.visibility = 'hidden';
+	teacherCard[i].addEventListener("click", flipteacherCard);
+	teacherCard[i].addEventListener("mousemove", moving);
+}
+
+teacherCard[0].style.visibility = 'visible';
 teacherCardBack.style.opacity = '0';
 teacherCardBack.style.zIndex = '-1';
 
-document.body.appendChild(teacherCardBack);
+body.appendChild(teacherCardBack);
 
-teacherCard[0].addEventListener("click", flipteacherCard);
-teacherCardBack.addEventListener("click", flipteacherCard);
+document.addEventListener("mousedown", function(event) {
+	mousedown = true;
+	mousedownX = event.clientX;
+});
+document.addEventListener("mouseup", function(event) {
+	mousedown = false;
+	mouseupX = event.clientX;
+});
 
-let isFlipped = false;
+function moving(event) {
+	if (mousedown) {
+		teacherCard[currentTeacher].style.transition = "transform 0s ease-in-out";
+		console.log(event.clientX);
+		teacherCard[currentTeacher].style.transform = "translate(" + (event.clientX - mousedownX) + "px, 0)";
+		if (mouseupX > mousedownX + dragActionDistance) {
+			changeTeacher();
+		} else {
+
+		}
+	}
+}
+
+function changeTeacher() {
+	teacherCard[currentTeacher].style.visibility = 'hidden';
+	
+	if (currentTeacher + 1 < teacherCard.length) {
+		currentTeacher++;
+	}
+
+	teacherCard[currentTeacher].style.visibility = 'visible';
+}
 
 function flipteacherCard() {
+	teacherCard[currentTeacher].style.transition = "all 0.5s ease-in-out, opacity 0s";
+
 	if (isFlipped) {
 		teacherCard[0].style.transform = 'rotateY(0deg)';
 		teacherCardBack.style.transform = 'rotateY(0deg)';
